405cbb90925e88d7c28e7d527d6fa56de04dd902
Mina Louis <36999501+minalouis9@users.noreply.github.com>
2019-02-25 22:26:36 +0200

    added Definitions.hpp

---
+++ src/Definitions.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+enum GameMode
+{
+	TEACHER_MODE,
+	AI_MODE
+};
+
+enum GamePhase
+{
+	MID_GAME_MODE,
+	PREEND_GAME_MODE,
+	END_GAME_MODE
+};
