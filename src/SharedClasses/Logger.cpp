96fd9f8bec9f556af24cff13e1c463ca9d3beed6
ahelhady511@gmail.com <ahelhady511@gmail.com>
2019-02-26 22:37:14 +0200

    changed implementation of Logger
    * to avoid I/O bound process, changed logger to work on standard console screen instead of file output.
    
    * In future work, this may be not very efficiency lagging issue. If this was agreed on, reverse back to log file output

--- src/SharedClasses/Logger.cpp
+++ src/SharedClasses/Logger.cpp
@@ -1,21 +1,9 @@
 #include "Logger.hpp"
 #include <string>
-#include <fstream>
 #include <iostream>
 
 
-void Logger::log(std::string message){ // log function begin
-    
-    std::ofstream logFile; // local scope instance of output file
-    logFile.open("log.txt",std::ios::app); // open log.txt file in write/append mode
-
-    if(logFile.is_open()){ // if begin
-        logFile<<message<<std::endl; // print the log message in a separate line
-        logFile.close(); // close file to free resources
-    } // if end
-    else{ // else begin
-        std::cout<<"Error: could NOT open log file...\n";
-    } // else end
-
+void Logger::log(std::string message){ // log function begin    
+    std::cout<<message<<std::endl; // use standard console screen instead of file output to avoid I/O bounded process
 } // log function end
 
