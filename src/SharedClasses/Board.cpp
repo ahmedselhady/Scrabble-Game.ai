1657d353679d7d4ac6976a57958e10a4063d882e
Ali Tolba <Ali_Ahmed97@hotmail.com>
2019-03-02 01:31:58 +0200

    Added header and cpp files for Board class.
    Features added:
    *Constructor
    *GetNextState function
    *GetTilesFromGUI function
    
    Please note that all data types are initial and will be changed
    according to the data types that will be used in the project and also
    the data members will be updated accordingly.

---
+++ src/SharedClasses/Board.cpp
@@ -0,0 +1,17 @@
+#include "Board.hpp"
+#include <iostream>
+#include <string>
+
+
+Board::Board() {
+
+}
+
+int Board::getNextState {
+	//will call the function from the AI module to determine the next state given the current board
+}
+
+int* Board::getTilesFromGUI{
+	//will call the GUI function which will aquire the move played by the player
+}
+
