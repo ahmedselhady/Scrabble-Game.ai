302481008794e4625f0839febe0ac9de2f1882b1
Andrew-Baher <37151807+Andrew-Baher@users.noreply.github.com>
2019-03-02 17:33:18 +0200

    Last changes

--- src/Board/Board_and_tiles/Board_and_tiles/BoardConst.h
+++ src/Board/Board_and_tiles/Board_and_tiles/BoardConst.h
@@ -1,25 +1,39 @@
-#pragma once
+#ifndef BOARDConstants_H
+#define BOARDConstants_H
+
 #include "uint256_t-master\uint256_t.h"
+#include <iostream>
+
+using namespace std;
+
 typedef uint256_t Bitboard;
+
 //000000000000000
-Bitboard w = 0b1000000100000010000000000000000000000000000000000000000000000000;
-Bitboard x = 0b0000000000000000000000000000000000000000010000000000000100000000;
-Bitboard y = 0b0000000000000000000000000000000000000000000000000000000000000000;
-Bitboard z = 0b000000000000000000100000010000001;
-Bitboard Wide(Bitboard w, Bitboard x, Bitboard y, Bitboard z)
+bool getBit(Bitboard byte, int position) // position in range 0-7
 {
-	Bitboard result;
-	result = w;
-	result << 64;
-	result = result | x;
-	result << 64;
-	result = result | y;
-	result << 64;
-	result = result | z;
-	return result;
+	return (byte >> position) & 0x1;
 }
 
 
-//Bitboard x = 0b10000000000000;
+Bitboard Wide(Bitboard w, uint64_t x, uint64_t y, uint64_t z)
+{
+	Bitboard result,a;
+	result |= 1ULL << 63;
+	a |= result << 63;
+	//result = result | w>>64;
+	//result = result | x;
+	//result << 64;
+	//result = result | y;
+	//result << 64;
+	//result = result | z;
+	return result;
+}
 
-//const Bitboard WSx3 = 0b;*/
+Bitboard w = 0b1000000100000010000000000000000000000000000000000000000000000000;
+uint64_t x = 0b0000000000000000000000000000000000000000010000000000000100000000;
+uint64_t y = 0b0000000000000000000000000000000000000000000000000000000000000000;
+uint64_t z = 0b000000000000000000100000010000001;
+Bitboard WSx3 = Wide(w, x, y, z);
+
+
+#endif
