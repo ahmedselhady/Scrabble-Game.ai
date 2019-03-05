3e312c1d82f7e01b663842b38c6dd64c025482d2
Andrew-Baher <37151807+Andrew-Baher@users.noreply.github.com>
2019-03-05 23:31:11 +0200

    Constants of scores multiples

--- src/Board/Board_and_tiles/Board_and_tiles/main.cpp
+++ src/Board/Board_and_tiles/Board_and_tiles/main.cpp
@@ -1,30 +1,8 @@
-#include "BoardMask.h"
+#include "BoardConst.h"
 #include <iostream>
 
 int main() {
 
-	//BoardMask bm;
-	BoardMask bm2(0,5, 5,0 );
-	BoardMask bm3(0, 0, 2, 0);
-
-	bm2 |= bm3 &=bm3;
-
-	bm3 = bm2;
-	//bm & bm2 | bm3;*/
-
-	/*BoardMask bm;
-
-	bm.setBit(16);
-	bm.setBit(100);
-	bm.setBit(160);
-	bm.setBit(196);
-	bm.setBit(90);
-IMP-main-code-implementation/board-and-tiles
-	bm.unsetBit(90);
-	bm.unsetBit(100);
-
-	std::cout << bm.getBit(160)<<" "<<bm.getBit(90)<<std::endl;
-*/
 	system("pause");
 	return 0;
 }
