056d6dbe006be744bd6849fe5c391b17365cf436
OmarOsaleh <Omar.Osaleeh@gmail.com>
2019-03-15 14:34:16 +0200

    Update BoardCommunication.h

--- src/Board/Board_and_tiles/Board_and_tiles/BoardCommunication.h
+++ src/Board/Board_and_tiles/Board_and_tiles/BoardCommunication.h
@@ -7,13 +7,13 @@ class BoardToGrammer: public BoardCommunicator {
 
 public:
 
-    BoardToGrammer();
-    ~BoardToGrammer();
+    BoardToGrammer();//Constructor
+    ~BoardToGrammer();//Destructor
 
     // overrided methods:
-    std::vector<char>& getNextVerticalState();
-    std::vector<char>& getNextHorizontalState();
-    std::vector<char>& getTiles();
+    std::vector<char>& getNextVerticalState();//it will call the function from the AI module to determine the next state given the current board Vertically
+    std::vector<char>& getNextHorizontalState();//it will call the function from the AI module to determine the next state given the current board Horizontally
+    std::vector<char>& getTiles();//it will call the GUI function which will aquire the move played by the player
 
 protected:
     // add any protected methods or data members
