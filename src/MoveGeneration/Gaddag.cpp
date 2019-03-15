864d7fe83d89a172a39e6c5177aca12f585d7744
OmarOsaleh <Omar.Osaleeh@gmail.com>
2019-03-15 14:01:14 +0200

    Update Gaddag.cpp

--- src/MoveGeneration/Gaddag.cpp
+++ src/MoveGeneration/Gaddag.cpp
@@ -22,6 +22,10 @@ Node::Node(){
 	nodeInfo = 0;
 }
 
+/*
+The Function findChildChar is given a letter it finds the child (domain of search only in this parent's childs)
+with this specific letter returns NULL if not found.
+*/
 Node* Node::findChildChar(char letter){
     Node*child = getFirstChild();
     cout<<std::hex<<this<<endl;
@@ -34,36 +38,43 @@ Node* Node::findChildChar(char letter){
     }
 
     return NULL; // not found.
-} // given a letter it finds the child (domain of search only in this parent's childs) with this specific letter returns NULL if not found.
+} 
 
+
+//The function getFirstChild it gets first childs uncompressed.
 inline Node* Node::getFirstChild(){
     int addressCastOffset = (nodeInfo & ADDRESS_OFFSET_CASTING);
     return addressCastOffset != 0 ? this + (addressCastOffset) : 0;
-} // Gets first childs uncompressed.
+}
 
+//The Function isLastChild the node is last character in a valid word.
 inline bool Node::isLastChild(){
     return ((nodeInfo & LAST_CHILD_OFFSET_CASTING) == LAST_CHILD_OFFSET_CASTING);
-} // i.e. Node is last character in a valid word.
+} 
 
+//The Function getNextChild it gets a successor in parent's childs.
 inline Node* Node::getNextChild(){
     if (!isLastChild()){
         return this + 1;
     }
     else {return 0;}
-} // Gets a successor in parent's childs.
+}  
 
+//Function getNodeLetter it retuns the node character.
 inline char Node::getNodeLetter(){
     if(((nodeInfo>>CHAR_SHIFT_VALUE) & CHAR_OFFSET_CASTING) ==  COMPRESSED_GADDAG_DELIMITER){
         return GADDAG_DELIMITER;
     }
     return (char)(CHAR_OFFSET + ((nodeInfo>>CHAR_SHIFT_VALUE) & CHAR_OFFSET_CASTING));
-} // Retuns the node character.
+} 
 
+//The Function getNodeInfo it returns node info.
 int Node :: getNodeInfo(){
 	return nodeInfo;
-}// returns node info.
+}
 
+//The Function isEndofWord the node is EOW .. end of word means a word or sub-word has been reached.
 inline bool Node::isEndOfWord(){
     return ((nodeInfo & END_OF_WORD_OFFSET_CASTING) == END_OF_WORD_OFFSET_CASTING);
- } // i.e. Node is EOW .. end of word means a word or sub-word has been reached.
+ } 
 
