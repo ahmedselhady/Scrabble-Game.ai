5d474141257b589f1073e6e59b88fc751ae70f07
Mohamed Bassel <48108775+mohamedbassel24@users.noreply.github.com>
2019-03-15 12:08:55 +0200

    Complete The 2Fun(Next Horizontal/Vertical)
    Now the 2 func is returning a vector of char indecting  the position of each  char

--- src/Board/Board_and_tiles/Board_and_tiles/Board.h
+++ src/Board/Board_and_tiles/Board_and_tiles/Board.h
@@ -15,6 +15,7 @@ public:
 	static Board* getBoard();
 	std::vector<char>& getNextHorizontal(int HorizontalIndex);
 	std::vector<char>& getNextVertical(int VerticalIndex);
+	void SetCharPos(char Letter,int Row,int Col);// This Func is to Set the bit for this char in board
 	int calculateScore(std::vector<char>& suggestedMove, int position, bool horizontal);
 
 
