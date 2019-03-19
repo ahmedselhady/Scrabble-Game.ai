d2ffd21031dbb41134607697530dbbbdf7520d1f
Amr Khaled <Amr.Soliman97@eng-st.cu.edu.eg>
2019-03-19 14:08:38 +0200

    Removed Buges in Opponent Rack Generator.
    -Removed Boost Library.

--- src/Opponent-Rack-Generator/Opponent.cpp
+++ src/Opponent-Rack-Generator/Opponent.cpp
@@ -1,9 +1,7 @@
 #pragma once
 
 #include "Opponent.h"
-
-// The Number of Distinct Letters in scrabble is: 26 English Letters + Blank:
-#define START_OF_WANTED_ASCII 49
+#include <utility>
 
 /*
 Written by: Amr Khaled
@@ -16,7 +14,7 @@ NOTE:
 
 This code takes as input:
 
- -Unordered Map of Pairs containing:
+  -Unordered Map of Pairs containing:
 
 	1- First: The 27 Letters of the scrabble Language. (26 English Letters + Blank)
 	2- Second: Number of remaining letters of each letter. ( Remaining Letters = 100 - My rack - Ground Tiles).
@@ -34,7 +32,7 @@ OpponentRack::OpponentRack() {
 
 
 //Function to get Number letters left: (Not in my hand and not on the board)
-int OpponentRack::LeftLetters(unordered_map<char, int> Letters) {
+int OpponentRack::LeftLetters(std::vector<std::pair<char,int>> Letters) {
 
 	//Get Number of Remaining Letters that aren't on board or in my hand:
 	int Number_Of_Remaining_Letters = 0;
@@ -48,8 +46,21 @@ int OpponentRack::LeftLetters(unordered_map<char, int> Letters) {
 }
 
 
+void OpponentRack::RemoveZeroProbabilities(std::vector<std::pair<char,int>> &Copy_Letters) {
+
+	for (auto it = begin(Copy_Letters); it != end(Copy_Letters);)
+	{
+		if (it->second == 0)
+		{
+			it = Copy_Letters.erase(it);
+		}
+		else
+			++it;
+	}
+}
+
 //Function to get probabilities of each letter:
-vector<double> OpponentRack::GetProbabilities(unordered_map<char, int>Letters, int Number_Of_Remaining_Letters) {
+vector<double> OpponentRack::GetProbabilities(std::vector<std::pair<char,int>>Letters, int Number_Of_Remaining_Letters) {
 
 	vector<double> Letter_Probabilities;
 	//Get Number of Remaining Letters that aren't on board or in my hand:
@@ -67,71 +78,83 @@ vector<double> OpponentRack::GetProbabilities(unordered_map<char, int>Letters, i
 }
 
 
+std::vector<std::pair<char,int>> copy(std::unordered_map<char,int>& map){
+	
+	std::vector<std::pair<char,int>> vec; 
+
+	for(auto iteratorI: map){
+		vec.push_back(*new std::pair<char,int>(iteratorI.first,iteratorI.second));
+	}
+	return vec;
+}
 
 //Function to create the rack to be used:
 vector<char> OpponentRack::RackGenerator(unordered_map<char, int> Letters) {
 	OpponentRack::Rack.clear();
 	//Taking a copy of letters vector:
-	unordered_map<char, int> Copy_Letters = Letters;
+	std::vector<std::pair<char,int>> Copy_Letters = copy(Letters);
 
-	//Boost Library:
-	//Applying Srand() but for Boost Library:
-	
-	boost::random_device RandomDevice;
-	boost::mt19937 Generator(RandomDevice); //Mersenne Random number generator: produce unsigned int (0 -> 2^w -1)
-  
+	//-------------------------------------------------------------------------------------------------------//
+	//Remove any element with prob of zero:
+	OpponentRack::RemoveZeroProbabilities(Copy_Letters);
+	//-------------------------------------------------------------------------------------------------------//
+
+
    //Data Members where data will be returned in.
 	vector<double> Letter_Probabilities; //Vector containing probability of finding a letter in a rack.
 	int Number_Of_Remaining_Letters; //Number of Remaining Letters in the Bag and Opponent Hand.
 
 	//Get Number of Remaining Letters that aren't on board or in my hand:
-	Number_Of_Remaining_Letters = OpponentRack::LeftLetters(Letters);
+	Number_Of_Remaining_Letters = OpponentRack::LeftLetters(Copy_Letters);
 
 	//Get Probability of each character being drawn from the bag:
-	Letter_Probabilities = OpponentRack::GetProbabilities(Letters, Number_Of_Remaining_Letters);
+	Letter_Probabilities = OpponentRack::GetProbabilities(Copy_Letters, Number_Of_Remaining_Letters);
+
+
+	//std::mt19937 Generator;
+	std::random_device Generator;
+
 
 	//Populate one rack:
-	for (auto Letter_Position : Copy_Letters) {
+	for (int i = 0; i < 7;i++) {
 
 		//Function that Generates integers based on their different probabilities:
-		boost::random::discrete_distribution<> Distribution(Letter_Probabilities);
+	    //boost::random::discrete_distribution<> Distribution(Letter_Probabilities);
+		std::discrete_distribution<int> Distribution(Letter_Probabilities.begin(),Letter_Probabilities.end());
 
-		//Position: Starts from:
-		// 'a' --> '{'
-		// 97  --> 123
 		//Distribution generates numbers from 0 --> 26:
-		int Position = Distribution(Generator) + START_OF_WANTED_ASCII;
-		
-		//NOTE: This IS Another Implementation.
-		//int Position = Distribution(RandomDevice) + START_OF_WANTED_ASCII;
-
-	   //Letter_Position: Holds the ascii for the generated Letter.
-		char Letter_Position;
-
-		if (Position == (123 -'0')) { //if (Position == '{'){
-			Position = 35;            //Position = '#'
-			Letter_Position = ((char)Position);
-		}
-		else {
-			//Else: Put the ascii of the current letter:
-			Letter_Position = ((char)Position) + '0';
-		}
+		int Letter_Ascii = Distribution(Generator);
 
 		//Push one randomly selected letter into the suggested opponent rack:
-		int LetterFinal = Copy_Letters[Letter_Position];
-		OpponentRack::Rack.push_back(Letter_Position);
+		//int LetterFinal = Letters[Letter_Ascii];
 
-		//Decrement Number of remaining instances of the character:
-		Copy_Letters[Letter_Position]--;
+		if (OpponentRack::Rack.size() >= 7)
+			return OpponentRack::Rack;
 
-		//Decrement remaining Number of all characters:
-		Number_Of_Remaining_Letters--;
+		//TODO: Add Comment
+		if (Copy_Letters[Letter_Ascii].second > 0) {
+			OpponentRack::Rack.push_back(Letter_Ascii);
 
+			//Decrement remaining Number of all characters:
+			Number_Of_Remaining_Letters--;
+
+			//Decrement Number of remaining instances of the character:
+			Copy_Letters[Letter_Ascii].second--;
+		}
+		else {
+
+		 RemoveZeroProbabilities(Copy_Letters);
+		
+		}
 		//Update New Probabilities of Letters after removing the current letter:
 		Letter_Probabilities = OpponentRack::GetProbabilities(Copy_Letters, Number_Of_Remaining_Letters);
 
 
 	}
+
+	//Change the rack from int to char:
+
+
 
 	return OpponentRack::Rack;
 }
