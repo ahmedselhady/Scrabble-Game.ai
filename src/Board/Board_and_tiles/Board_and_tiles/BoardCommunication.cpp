d1284e27d4a05cc53e33a1a6c831ae8b7048ffc0
OmarOsaleh <Omar.Osaleeh@gmail.com>
2019-03-21 12:31:19 +0200

    Update BoardCommunication.cpp

--- src/Board/Board_and_tiles/Board_and_tiles/BoardCommunication.cpp
+++ src/Board/Board_and_tiles/Board_and_tiles/BoardCommunication.cpp
@@ -2,35 +2,38 @@
 #include "./BoardCommunication.h"
 #include "./Board.h"
 
-
+//Constructor
 BoardToGrammer::BoardToGrammer() {
 	BoardCommunicator::BoardPtr=Board::getBoard();// need to edit this 
 	BoardCommunicator::TilesPtr=Tiles::getTiles();// need to edit this 
 }
-
+//Destructor
 BoardToGrammer::~BoardToGrammer()
 {
 }
 
+//This Function it will call the function from the AI module to determine the next state given the current board Vertically
 std::vector<char>& BoardToGrammer::getNextVerticalState() {
 	//will call the function from the AI module to determine the next state given the current board
 	return (BoardPtr->getNextVertical(VerticalIndex++));
 }
 
+//This Function it will call the function from the AI module to determine the next state given the current board Horizontally
 std::vector<char>& BoardToGrammer::getNextHorizontalState(){
 	return (BoardPtr->getNextHorizontal(HorizontalIndex++)); // what if reached to 15?
 }
 
+//This Function will call the GUI function which will aquire the move played by the player
 std::vector<char>& BoardToGrammer::getTiles(){
-	//will call the GUI function which will aquire the move played by the player
 	return (* new std::vector<char>(TilesPtr->getRackTiles()));
 }
-
+//This Function will get the tile that is in the given rows and coloumns
 char BoardToGrammer::getTileAtPosition(int row, int col) {
 	vector<char> CurrentRow = BoardPtr->getNextHorizontal(row);
 	return (CurrentRow[col] == '*') ? '*' : CurrentRow[col];
 }
 
+//This function set character postion
 void BoardToGrammer::SetChar(char Letter, int Row, int Col)
 {
 	BoardPtr->SetCharPos(Letter, Row, Col);
@@ -41,7 +44,7 @@ void BoardToGrammer::PrintBitBoard()
 	BoardPtr->print();
 }
 
-
+//This function checs if this place contain a tile or not
 bool BoardToGrammer::hasaTile(int row, int col){     
 	vector<char> CurrentRow=BoardPtr->getNextHorizontal(row);
 	return (CurrentRow[col] == '*') ? false : true;
