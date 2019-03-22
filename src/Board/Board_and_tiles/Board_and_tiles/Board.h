0f11cd0720997ead187e8e6efa6ae155216b08a5
OmarOsaleh <Omar.Osaleeh@gmail.com>
2019-03-22 22:17:35 +0200

    Update Board.h

--- src/Board/Board_and_tiles/Board_and_tiles/Board.h
+++ src/Board/Board_and_tiles/Board_and_tiles/Board.h
@@ -16,14 +16,14 @@ private:
 
  
 public:
-	static Board* getBoard();
+	static Board* getBoard();//it gets instance of the board
 	
-	std::vector<char>& getNextHorizontal(int HorizontalIndex);
-	std::vector<char>& getNextVertical(int VerticalIndex);
+	std::vector<char>& getNextHorizontal(int HorizontalIndex);//it will call the function from the AI module to determine the next state given the current board Horizontally
+	std::vector<char>& getNextVertical(int VerticalIndex);//it will call the function from the AI module to determine the next state given the current board Vertically
 	void SetCharPos(char Letter,int Row,int Col);// This Func is to Set the bit for this char in board
-	int calculateScore(std::vector<char>& suggestedMove, int position, bool horizontal);
+	int calculateScore(std::vector<char>& suggestedMove, int position, bool horizontal);//it calculates the word and letter score
 	void print();
-	~Board();
+	~Board();//Destructor
 
 
 };
