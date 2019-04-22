3cf321a9066b157ad3c016dec662862fba4a6bf1
ahmedHusseinF <hussein.ahmed50@gmail.com>
2019-04-22 22:56:16 +0200

    added states handling and refacoring play determination

--- src/Communication/ScrabbleClient.cpp
+++ src/Communication/ScrabbleClient.cpp
@@ -11,47 +11,138 @@
 using easywsclient::WebSocket;
 static WebSocket::pointer ws = NULL;
 
-void handle_message(std::vector<uint8_t>& message) {
+// this is used to hold the current state of our communcation
+States state = States::INIT;
+
+void func() {}
+
+void handle_message(const std::vector<uint8_t>& message) {
   // the message type always exist in the first
   // byte of the message
   uint8_t msgType = message[0];
 
+  // printing the buffer just for debugging
+  // TODO: Remove this when finished the game
+  std::cout << "Received buffer: ";
+  for (auto& m : message) {
+    std::cout << std::to_string(m) << ", ";
+  }
+  std::cout << std::endl;
+
   // always send the envelope unless it doesn't matter
   bool sendMessage = true;
+  // this should be true when we have to make a play then send it
   BinaryEnvelope env;
 
-  /**
-   * ! This switch case will be very convulted real soon
-   * TODO: Should think how to do it in a better way
-   */
-  switch (msgType) {
-    case MessageTypes::NAME:
-      // The server is expecting a 0 response to a "Name" request
-      env.insertUInt8(0);
-      env.insertString("Wermacht 卐");  // Send our name hardcoded :D
-      break;
-    /*****************************************************************************/
-    case MessageTypes::START:
-      auto msg = deserializeReadyMessage(message);
+  // * No switch case because it doesn't accept a variable in the case block
+  if (msgType == MessageTypes::NAME) {
+    // The server is expecting a 0 response to a "Name" request
+    if (state != States::INIT) {
+      std::cerr << "Received Name but I have state: " << state << std::endl;
+    }
+    env.insertUInt8(MessageTypes::NAME);
+    env.insertString("Wermacht");  // Send our name hardcoded :D
+    state = States::READY;
+  } else if (msgType == MessageTypes::START) {
+    auto smsg = deserializeReadyMessage(message);
+    // TODO: Construct the board and hold it from here
 
-      if (msg.order = 1) {
-        // well this is our turn
-      }
+    if (smsg.order = 1) {
+      // * well this is our turn
+      state = States::THINKING;
+    } else {
+      // ? we are idle, this is the opponent's move
+      state = States::IDLE;
+    }
+  } else if (msgType == MessageTypes::END) {
+    std::cout << "An end packet was received... losing the connection ";
 
-      break;
-      /*****************************************************************************/
-    case MessageTypes::END:
-      std::cout << "An end packet was received... losing the connection";
-      auto msg = deserializeEndMessage(message);
-      std::cout << "Our score is " << msg.ourScore
-                << ", the other player's score is " << msg.opponentScore
-                << std::endl;
+    auto emsg = deserializeEndMessage(message);
+    std::cout << "Our score is " << emsg.ourScore
+              << ", the other player's score is " << emsg.opponentScore
+              << std::endl;
 
-      ws->close();
-      break;
-    default:
-      std::cerr << "Unknown Message Type" << std::endl;
-      break;
+    state = States::READY;
+  } else if (msgType == MessageTypes::PASS) {
+    auto passFromOpponent = deserializePassMessage(message);
+
+    // other player passed, we should play
+    state = States::THINKING;
+
+    // ? state should be known after the play is known
+  } else if (msgType == MessageTypes::PLAY) {
+    auto playFromOpponent = deserializePlayMessage(message);
+    // TODO: here we can update the gui maybe
+    state = States::AWAIT_AGENT_CHALLENGE;
+  } else if (msgType == MessageTypes::INVALID) {
+    if (state == States::AWAIT_PLAY_RESPONSE) {
+      auto invalidPlay = deserializeInvalidPlayMessage(message);
+    } else if (state == States::AWAIT_EXCHANGE_RESPONSE) {
+      auto invalidEx = deserializeInvalidExchangeMessage(message);
+      state = States::THINKING;
+    } else {
+      std::cerr << "Undefined state and action Invalid and state: " << state;
+    }
+    std::cout << "Received an Invalid message, won't do anything" << std::endl;
+  } else if (msgType == MessageTypes::CHALLENGE_ACCEPTED) {
+    auto chgmsg = deserializeChallengeAcceptedMessage(message);
+    // * we won the challenge we should play as well
+    if (state == States::AWAIT_CHALLENGE_RESPONSE) {
+      state = States::THINKING;
+    } else if (state == States::AWAIT_PLAY_RESPONSE) {
+      state = States::IDLE;
+    }
+
+    // TODO: maybe update the gui
+  } else if (msgType == MessageTypes::CHALLENGE_REJECTED) {
+    auto chgrej = deserializeChallengeRejectedMessage(message);
+
+    if (state == States::AWAIT_CHALLENGE_RESPONSE) {
+      state = States::IDLE;
+    } else if (state == States::AWAIT_PLAY_RESPONSE) {
+      state = States::THINKING;
+    }
+
+    // TODO: update the gui
+  } else if (msgType == MessageTypes::NO_CHALLENGE) {
+    auto nochg = deserializeNoChallengeMessage(message);
+    if (state == States::AWAIT_CHALLENGE_RESPONSE) {
+      state = States::THINKING;
+    } else if (state == States::AWAIT_AGENT_CHALLENGE) {
+      state = States::THINKING;
+    }
+  } else if (msgType == MessageTypes::EXCHANGE) {
+    if (state == States::AWAIT_EXCHANGE_RESPONSE) {
+      auto msg = deserializeOkExchangeMessage(message);
+      // TODO: now i have new tiles, should update our agent
+      state = States::IDLE;
+    } else if (state == States::IDLE) {
+      // * other player has exchanged, now we play
+      state = States::THINKING;
+    } else {
+      std::cerr
+          << "Undefined state and action received Exchange, current state: "
+          << state;
+    }
+  } else if (msgType == MessageTypes::CHALLENGE) {
+  } else {
+    // ? this is the default if the message is weird
+    std::cerr << "Unknown Message Type, ya bashmo7nds zbt acoadk" << std::endl;
+  }
+
+  if (state == States::AWAIT_AGENT_CHALLENGE) {
+    // * we are challenging, huh ?
+    // if challenging
+    // then await_challenge_response
+    // else thinking
+  }
+
+  if (state == States::THINKING) {
+    // clear the envelope anyway because I might have polluted it
+    // TODO: playing should modify state, WIP
+    // * should investigate doing an async task right here
+    env.clear();
+    env.insertUInt8(MessageTypes::PASS);  // PASS all the moves for now
   }
 
   // finished the message, if we need to send then send it
@@ -61,30 +152,24 @@ void handle_message(std::vector<uint8_t>& message) {
 }
 
 int main(int argc, char** argv) {
-  std::string hostname;
+  // use the default local machine socket
+  std::string hostname = "ws://localhost:8080/";
 
   if (argc > 1) {
     hostname = argv[1];
-  } else {
-    std::cout << "Enter the socket to connect to: ";
-    std::cin >> hostname;
   }
 
-  // use the default local machine socket
-  if (hostname.empty()) {
-    hostname = "ws://localhost:8080/";
-  }
-
-  // amount of connection retries to the server
+  // * amount of connection retries to the server
   int retries = 0;
 
   std::cout << "Will try to connect to " << hostname << std::endl;
 
   while (true) {
-    // try to connect to the host using the library
+    // * try to connect to the host using the library
     ws = WebSocket::from_url(hostname);
+    ws->sendPing();
 
-    // make sure we got the connection, if not fail and exit
+    // ! make sure we got the connection, if not fail and exit
     assert(ws);
 
     // receive binary message as long as there is connection
@@ -95,7 +180,11 @@ int main(int argc, char** argv) {
 
       // here we are chillin'
       // do something non-blocking !!!!
+      // TODO: maybe send a non-blocking message to the GUI, maybe ?
     }
+
+    // * Reset the state to start a new connection
+    state = States::INIT;
 
     delete ws;
 
