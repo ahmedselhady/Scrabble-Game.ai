1385baebccc0a021556c90473a65fac9633e03ab
Amr Khaled <Amr.Soliman97@eng-st.cu.edu.eg>
2019-04-27 13:20:52 +0200

    AI_MODE
    Added some changes but will further change it now.

--- src/AI_MODE.cpp
+++ src/AI_MODE.cpp
@@ -1,28 +1,44 @@
 #pragma once
 
 #include "AI_MODE.h"
-#include <utility>
 
 /*
 Written By: Amr Khaled
 
 Note for Developers: 20/4/2019
+
 - Include Board, I don't know where to find it that is why iput it as int to elemenate errors only..
 - Assuming that unordered_map<char,int> Tiles contains all tiles that aren't played yet after removing our tiles.
 -
 
 */
+
 //Constructor for Ai_Mode:
+inline Node* createGaddag(){ // just a test function.
+	 LoadGaddag gaddagConstructor;
+	 Node* gaddagRoot = gaddagConstructor.constructGaddag();
+	 return gaddagRoot;
+}
 
-AI_MODE::AI_MODE(int Board ,unordered_map<char,int> Tiles,unordered_map<char,int> OurRack ,int OurScore,int TheirScore,time_t t) {
-        
-////////////////////////////////////////////////////////////
-/*To be Added:
-    -Generate Moves Function here
+AI_MODE::AI_MODE(BoardToGrammer* B,unordered_map<char,int> Tiles, vector<char>& Rack) {
+////////////////////////////////////////////
 
-*/
-////////////////////////////////////////////////////////////        
-list<Move> L;
+
+Node* gaddagRoot;
+gaddagRoot = createGaddag(); // THIS FUCNTION ONLY USED FOR TESTING.
+WordGenerate * Gen = new WordGenerate(B,gaddagRoot);
+
+
+Gen->countTilesRack(&Rack); 
+
+// Next line or the following two:
+//  Gen->emptyBoardMoves();
+Gen->crosssets(); // Gen->Updatecrosssets();
+Gen->generateWords();
+
+list<Move> L = Gen->allMoves();
+
+////////////////////////////////////////////////
 OpponentRack OP;
 AI_MODE::TheOpponentRack = OP.RackGenerator(Tiles);
 int TilesLeft =0;
@@ -35,18 +51,16 @@ int TilesLeft =0;
 // Create the Monte Carlo Search Tree:
 MCTSearch MonteCarlo(L);
 
-
-//NOTE: I don't remember the exact number from which i should change from MidGame to EndGame so i assumed 10:
 int Index = 0; //Index of best Move in Moves Lists.
 
 //Get Game Phase:
-if(TilesLeft > 10){
+if(TilesLeft > 9){
 
 Index = MonteCarlo.midGameMCTS();
 
 }else{
 
-Index = MonteCarlo.midGameMCTS();
+Index = MonteCarlo.endGameMCTS();
 
 }
 
@@ -78,4 +92,13 @@ vector<char> AI_MODE::getOpponentRack(){
 Move AI_MODE::getBestMove(){
 
     return AI_MODE::BestMove;
+}
+
+
+
+Move AI_Mode_Function(BoardToGrammer* B,unordered_map<char,int> Tiles, vector<char>& Rack){
+    
+    AI_MODE* AI = new AI_MODE(B,Tiles,Rack);
+    Move M = AI->getBestMove();
+    return M;
 }
--- src/AI_MODE.h
+++ src/AI_MODE.h
@@ -2,10 +2,25 @@
 #include <vector>
 #include <ctime>
 #include <list>
-#include "Opponent-Rack-Generator/Opponent.h"
 #include <unordered_map>
 #include <math.h> 
+#include <utility>
+
+#include "Board/Board_and_tiles/Board_and_tiles/Board.h";
+#include "Board/Board_and_tiles/Board_and_tiles/BoardCommunication.h";
+#include "Board/Board_and_tiles/Board_and_tiles/Tiles.h";
+#include "Opponent-Rack-Generator/Opponent.h"
 #include "SharedClasses/MCTSearch.h"
+#include "../src/MoveGeneration/LoadGaddag.h"
+#include "../src/MoveGeneration/MoveGenerate.h"
+#include "../src/MoveGeneration/Gaddag.h"
+#include <algorithm>
+#include <string> 
+#include "../src/Board/Board_and_tiles/Board_and_tiles/Tiles.h"
+#include "../src/Board/Board_and_tiles/Board_and_tiles/BoardCommunication.h"
+#include <iostream>
+#include <cctype>
+
 using namespace std;
 
 
@@ -13,15 +28,16 @@ class AI_MODE {
 
 public:
 
-    AI_MODE();
-  AI_MODE(int Board,unordered_map<char,int> ,unordered_map<char,int>  ,int,int,time_t);
+    AI_MODE(BoardToGrammer* ,unordered_map<char,int>, vector<char>&);
     vector<char> getMyRack();
     vector<char> getOpponentRack();
     Move getBestMove();
 private:
 
-	vector<char> MyRack;
+  	vector<char> MyRack;
     vector<char> TheOpponentRack;
     Move BestMove;
 	///////////////////////////////////////////////////////////////
 };
+
+Move AI_Mode_Function(BoardToGrammer* B,unordered_map<char,int> Tiles, vector<char>& Rack);
