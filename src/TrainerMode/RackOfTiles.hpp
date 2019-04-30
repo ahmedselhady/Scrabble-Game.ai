fa48de26a2ff41a3dc59eaf3ad7986a948df5160
Andrew-Baher <37151807+Andrew-Baher@users.noreply.github.com>
2019-04-30 15:45:12 +0200

    communicator added to Rack

--- src/TrainerMode/RackOfTiles.hpp
+++ src/TrainerMode/RackOfTiles.hpp
@@ -2,14 +2,17 @@
 
 #include <unordered_map>
 #include <iostream>
+#include <string>
+#include"../SharedClasses/TrainerComm.hpp"
 
 class RackOfTiles
 {
 private:
     std::unordered_map<char, int> * bag;
     int bagSize;
+    TrainerComm* Communicator;
 public:
-    RackOfTiles(std::unordered_map<char, int> * Bag,int BagSize);
+    RackOfTiles(std::unordered_map<char, int> * Bag,int BagSize,TrainerComm* Comm);
     ~RackOfTiles();
     bool updateBag(char tileToReduce);
     std::vector<char> RandomizeTiles(int WantedTiles);
